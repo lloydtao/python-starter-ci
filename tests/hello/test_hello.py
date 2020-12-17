9e84d72f4a9209bc9f7718f2c93f7c1b99d93337
LloydTao <LL23@live.co.uk>
2020-12-17 04:36:57 +0000

    Wrap tests in a test class

--- tests/hello/test_hello.py
+++ tests/hello/test_hello.py
@@ -7,22 +7,23 @@ import pytest
 from project.hello import hello, main
 
 
-def test_hello():
-    """Test hello() with the default subject ("world")."""
-    assert hello() == "Hello, world!"
+class TestHelloManager:
+    """Test methods within the `hello` module."""
 
+    def test_hello(self):
+        """Test hello() with the default subject ("world")."""
+        assert hello() == "Hello, world!"
 
-def test_hello_input():
-    """Test hello() with an argument for the subject of the greeting."""
-    assert hello("test") == "Hello, test!"
+    def test_hello_input(self):
+        """Test hello() with an argument for the subject of the greeting."""
+        assert hello("test") == "Hello, test!"
 
+    def test_main(self, capsys):
+        """Test when the module is used as a script.
 
-def test_main(capsys):
-    """Test when the module is used as a script.
-
-    Args:
-        capsys (obj): Use stdout without needing to set/reset the output stream.
-    """
-    main("main")
-    captured = capsys.readouterr()
-    assert captured.out == "Hello, main!\n"
+        Args:
+            capsys (obj): Use stdout without needing to set/reset the output stream.
+        """
+        main("main")
+        captured = capsys.readouterr()
+        assert captured.out == "Hello, main!\n"
