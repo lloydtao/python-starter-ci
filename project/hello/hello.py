ece85405a387619fa689d8b74fccda53837dbe86
LloydTao <LL23@live.co.uk>
2020-12-17 04:34:52 +0000

    Add documentation to sample module

--- project/hello/hello.py
+++ project/hello/hello.py
@@ -7,16 +7,30 @@ import argparse
 
 
 def hello(s="world"):
+    """Generate a string that greets the subject (i.e. "Hello, world!").
+
+    Args:
+        s (str, optional): The subject of the greeting. Defaults to "world".
+
+    Returns:
+        str: A friendly message to greet the subject.
+    """
     message = "Hello, " + s + "!"
     return message
 
 
 def main(word):
+    """Print a friendly greeting to the user's command line.
+
+    Args:
+        word (str): The subject of the greeting.
+    """
     output = hello(word)
     print(output)
 
 
 if __name__ == "__main__":
+    """Run the module as a script."""
 
     # Parse command line arguments
     parser = argparse.ArgumentParser()
--- tests/hello/test_hello.py
+++ tests/hello/test_hello.py
@@ -1,18 +1,28 @@
 #!/bin/python3
+"""
+Unit tests for the `hello` module.
+"""
 import pytest
 
 from project.hello import hello, main
 
 
 def test_hello():
+    """Test hello() with the default subject ("world")."""
     assert hello() == "Hello, world!"
 
 
 def test_hello_input():
+    """Test hello() with an argument for the subject of the greeting."""
     assert hello("test") == "Hello, test!"
 
 
 def test_main(capsys):
+    """Test when the module is used as a script.
+
+    Args:
+        capsys (obj): Use stdout without needing to set/reset the output stream.
+    """
     main("main")
     captured = capsys.readouterr()
     assert captured.out == "Hello, main!\n"
